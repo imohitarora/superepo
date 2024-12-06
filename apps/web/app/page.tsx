874e516a8b38b88b81f3814fa1d66bea7e5ecff0
Mohit Arora <me@aboutmohit.com>
2024-12-06 13:35:57 -0500

    updated the theme

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,8 @@
-import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello, world!</h1>
-      <Button>Click me</Button>
     </div>
   );
 }
