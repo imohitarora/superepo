a9eb5c9a08f2b8efdb13b7709079d9c398b3682f
Mohit Arora <mohitarora.j2ee@gmail.com>
2025-02-07 12:05:09 -0500

    sidebar cleanup

--- apps/web/components/app-sidebar.tsx
+++ apps/web/components/app-sidebar.tsx
@@ -1,21 +1,18 @@
 "use client"
 
-import * as React from "react"
 import {
   AudioWaveform,
-  BookOpen,
-  Bot,
   Command,
   Frame,
   GalleryVerticalEnd,
+  Home,
   Map,
   PieChart,
-  Settings2,
-  SquareTerminal,
+  Settings2
 } from "lucide-react"
+import * as React from "react"
 
 import { NavMain } from "@/components/nav-main"
-import { NavProjects } from "@/components/nav-projects"
 import { NavUser } from "@/components/nav-user"
 import { TeamSwitcher } from "@/components/team-switcher"
 import {
@@ -52,9 +49,9 @@ const data = {
   ],
   navMain: [
     {
-      title: "Playground",
+      title: "Home",
       url: "#",
-      icon: SquareTerminal,
+      icon: Home,
       isActive: true,
       items: [
         {
@@ -65,73 +62,27 @@ const data = {
           title: "Transactions",
           url: "/transactions",
         },
-        {
-          title: "Settings",
-          url: "/settings",
-        },
-      ],
-    },
-    {
-      title: "Models",
-      url: "#",
-      icon: Bot,
-      items: [
-        {
-          title: "Genesis",
-          url: "#",
-        },
-        {
-          title: "Explorer",
-          url: "#",
-        },
-        {
-          title: "Quantum",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Documentation",
-      url: "#",
-      icon: BookOpen,
-      items: [
-        {
-          title: "Introduction",
-          url: "#",
-        },
-        {
-          title: "Get Started",
-          url: "#",
-        },
-        {
-          title: "Tutorials",
-          url: "#",
-        },
-        {
-          title: "Changelog",
-          url: "#",
-        },
       ],
     },
     {
       title: "Settings",
-      url: "#",
+      url: "/settings",
       icon: Settings2,
       items: [
         {
-          title: "General",
+          title: "Profile",
           url: "#",
         },
         {
-          title: "Team",
+          title: "Security",
           url: "#",
         },
         {
-          title: "Billing",
+          title: "Team Members",
           url: "#",
         },
         {
-          title: "Limits",
+          title: "Invite",
           url: "#",
         },
       ],
@@ -164,7 +115,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       </SidebarHeader>
       <SidebarContent>
         <NavMain items={data.navMain} />
-        <NavProjects projects={data.projects} />
+        {/* <NavProjects projects={data.projects} /> */}
       </SidebarContent>
       <SidebarFooter>
         <NavUser user={data.user} />
