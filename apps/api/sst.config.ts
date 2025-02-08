7d8d37bace662359423c7b5895e251e747d53686
Mohit Arora <me@aboutmohit.com>
2025-02-08 13:08:28 -0500

    added sst

--- apps/api/sst.config.ts
+++ apps/api/sst.config.ts
@@ -3,11 +3,23 @@
 export default $config({
   app(input) {
     return {
-      name: "api",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      protect: ["production"].includes(input?.stage),
-      home: "aws",
+      name: 'api',
+      removal: input?.stage === 'production' ? 'retain' : 'remove',
+      // protect: ['production'].includes(input?.stage),
+      home: 'aws',
     };
   },
-  async run() {},
+  async run() {
+    const vpc = new sst.aws.Vpc('SupeVpc');
+    const cluster = new sst.aws.Cluster('SupeCluster', { vpc });
+
+    cluster.addService('SupeAPIService', {
+      loadBalancer: {
+        ports: [{ listen: '80/http', forward: '4000/http' }],
+      },
+      dev: {
+        command: 'pnpm dev',
+      },
+    });
+  },
 });
